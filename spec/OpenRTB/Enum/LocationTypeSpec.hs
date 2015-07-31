5fa9ce1fd9e7a7f2cccdbc4122972c06f14ae519
Justin Wood <justin.k.wood@gmail.com>
2015-07-31 17:33:13 -0400

    JSON instances for LocationType

---
+++ spec/OpenRTB/Enum/LocationTypeSpec.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+module OpenRTB.Enum.LocationTypeSpec where
+
+import Control.Applicative
+import Data.Aeson
+import Data.Aeson.TH
+import Test.Hspec
+import Test.QuickCheck
+import OpenRTB.Enum.LocationType
+
+data Mock = Mock { lt :: LocationType } deriving (Eq, Show)
+$(deriveJSON defaultOptions ''Mock)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "LocationType" $ do
+  context "JSON" $ do
+    it "should convert back and forth" $ property $ do
+      \m -> (decode . encode) m == Just (m :: Mock)
+
+    context "ToJSON" $ do
+      it "should properly encode a value" $ do
+        encode (Mock GPS) `shouldBe` "{\"lt\":1}"
+        encode (Mock UserProvided) `shouldBe` "{\"lt\":3}"
+
+    context "FromJSON" $ do
+      it "should properly decode a value" $ do
+        decode "{\"lt\":3}" `shouldBe` (Just (Mock UserProvided))
+        decode "{\"lt\":2}" `shouldBe` (Just (Mock IP))
+
+      it "should fail when out of range" $ do
+        decode "{\"lt\":0}" `shouldBe` (Nothing :: Maybe Mock)
+        decode "{\"lt\":4}" `shouldBe` (Nothing :: Maybe Mock)
+
+
+instance Arbitrary Mock where
+  arbitrary = Mock <$> arbitrary
+
+instance Arbitrary LocationType where
+  arbitrary = toEnum <$> choose (1, 3)
--- src/OpenRTB/Enum/LocationType.hs
+++ src/OpenRTB/Enum/LocationType.hs
@@ -1,5 +1,9 @@
 module OpenRTB.Enum.LocationType where
 
+import Control.Monad
+import Data.Aeson
+import Data.Scientific
+
 -- | The following table lists the options to indicate how the geographic
 --   information was determined.
 data LocationType =
@@ -11,3 +15,22 @@ data LocationType =
 
     -- | User provided (e.g., registration data)
   | UserProvided
+  deriving (Show, Eq)
+
+instance Enum LocationType where
+  toEnum 1 = GPS
+  toEnum 2 = IP
+  toEnum 3 = UserProvided
+
+  fromEnum GPS = 1
+  fromEnum IP = 2
+  fromEnum UserProvided = 3
+
+instance FromJSON LocationType where
+  parseJSON (Number i) =
+    case floatingOrInteger i of
+      Right n | 1 <= n && n <= 3 -> return (toEnum n)
+      _ -> mzero
+
+instance ToJSON LocationType where
+  toJSON lt = Number (scientific (fromIntegral $ fromEnum lt) 0)
