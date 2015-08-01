051f928ec12f8e94d359d9b0f74930502514a833
Justin Wood <justin.k.wood@gmail.com>
2015-08-01 12:05:58 -0400

    JSON instances for VideoLinearity

--- src/OpenRTB/Enum/VideoLinearity.hs
+++ src/OpenRTB/Enum/VideoLinearity.hs
@@ -1,5 +1,9 @@
 module OpenRTB.Enum.VideoLinearity where
 
+import Control.Monad
+import Data.Aeson
+import Data.Scientific
+
 -- | The following table indicates the options for video linearity. "In-stream"
 --   or "linear" video refers to pre-roll, post-roll, or mid-roll video ads
 --   where the user is forced to watch ad in order to see the video content.
@@ -24,3 +28,20 @@ data VideoLinearity =
 
     -- | Non-Linear / Overlay
   | NonLinear
+  deriving (Show, Eq)
+
+instance Enum VideoLinearity where
+  toEnum 1 = Linear
+  toEnum 2 = NonLinear
+
+  fromEnum Linear = 1
+  fromEnum NonLinear = 2
+
+instance FromJSON VideoLinearity where
+  parseJSON (Number i) =
+    case floatingOrInteger i of
+      Right n | 1 <= n && n <= 2 -> return (toEnum n)
+      _ -> mzero
+
+instance ToJSON VideoLinearity where
+  toJSON vl = Number (scientific (fromIntegral $ fromEnum vl) 0)
