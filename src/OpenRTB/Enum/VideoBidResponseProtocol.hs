8a8e1623a12b61d64b18aec52346a2aefcb4c7c3
Justin Wood <justin.k.wood@gmail.com>
2015-08-01 12:05:41 -0400

    JSON instances for VideoBidresponseProtocols

--- src/OpenRTB/Enum/VideoBidResponseProtocol.hs
+++ src/OpenRTB/Enum/VideoBidResponseProtocol.hs
@@ -1,5 +1,9 @@
 module OpenRTB.Enum.VideoBidResponseProtocol where
 
+import Control.Monad
+import Data.Aeson
+import Data.Scientific
+
 -- | The following table lists the options for video bid response protocols
 --   that could be supported by an exchange.
 data VideoBidResponseProtocol =
@@ -21,3 +25,28 @@ data VideoBidResponseProtocol =
 
     -- | Vast 3.0 Wrapper
   | Vast30Wrapper
+  deriving (Show, Eq)
+
+instance Enum VideoBidResponseProtocol where
+  toEnum 1 = Vast10
+  toEnum 2 = Vast20
+  toEnum 3 = Vast30
+  toEnum 4 = Vast10Wrapper
+  toEnum 5 = Vast20Wrapper
+  toEnum 6 = Vast30Wrapper
+
+  fromEnum Vast10 = 1
+  fromEnum Vast20 = 2
+  fromEnum Vast30 = 3
+  fromEnum Vast10Wrapper = 4
+  fromEnum Vast20Wrapper = 5
+  fromEnum Vast30Wrapper = 6
+
+instance FromJSON VideoBidResponseProtocol where
+  parseJSON (Number i) =
+    case floatingOrInteger i of
+      Right n | 1 <= n && n <= 6 -> return (toEnum n)
+      _ -> mzero
+
+instance ToJSON VideoBidResponseProtocol where
+  toJSON vbrp = Number (scientific (fromIntegral $ fromEnum vbrp) 0)
