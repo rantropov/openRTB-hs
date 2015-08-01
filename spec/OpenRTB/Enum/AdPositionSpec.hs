32012aa42c5cf17c9d36699080138f18601ac9ac
Justin Wood <justin.k.wood@gmail.com>
2015-08-01 16:56:29 -0400

    JSON instances for AdPosition

---
+++ spec/OpenRTB/Enum/AdPositionSpec.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+module OpenRTB.Enum.AdPositionSpec where
+
+import Control.Applicative
+import Data.Aeson
+import Data.Aeson.TH
+import Test.Hspec
+import Test.QuickCheck
+import OpenRTB.Enum.AdPosition
+
+data Mock = Mock { ap :: AdPosition } deriving (Eq, Show)
+$(deriveJSON defaultOptions ''Mock)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "AdPosition" $ do
+  context "JSON" $ do
+    it "should convert back and forth" $ property $ do
+      \m -> (decode . encode) m == Just (m :: Mock)
+
+    context "ToJSON" $ do
+      it "should properly encode a value" $ do
+        encode (Mock Unknown) `shouldBe` "{\"ap\":0}"
+        encode (Mock FullScreen) `shouldBe` "{\"ap\":7}"
+
+    context "FromJSON" $ do
+      it "should properly decode a value" $ do
+        decode "{\"ap\":3}" `shouldBe` (Just (Mock BelowTheFold))
+        decode "{\"ap\":2}" `shouldBe` (Just (Mock DEPRECATED))
+
+      it "should fail when out of range" $ do
+        decode "{\"ap\":-1}" `shouldBe` (Nothing :: Maybe Mock)
+        decode "{\"ap\":8}" `shouldBe` (Nothing :: Maybe Mock)
+
+
+instance Arbitrary Mock where
+  arbitrary = Mock <$> arbitrary
+
+instance Arbitrary AdPosition where
+  arbitrary = toEnum <$> choose (0, 7)
